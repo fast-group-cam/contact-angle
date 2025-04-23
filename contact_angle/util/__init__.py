7e1ae263aa7797408562dfc75e1b0bc39c3eef27
dwl38 <dwl38@cam.ac.uk>
2025-04-23 14:52:22 +0100

    Added timekeeping utility to contact_angle.util

--- contact_angle/util/__init__.py
+++ contact_angle/util/__init__.py
@@ -1,1 +1,18 @@
-from .droplet.center_coordinates import center_coordinates
+import time
+from .droplet.center_coordinates import center_coordinates, read_droplet_trajectory
+
+def elapsed_time(time_start: float) -> str:
+    """Given a start time measured by `time.time()`, this function measures the time elapsed since
+and formats it nicely into a string."""
+    time_taken = time.time() - time_start
+    if time_taken > 3600:
+        hours = int(time_taken / 3600)
+        time_taken -= hours * 3600
+        mins = int(time_taken / 60)
+        time_taken -= mins * 60
+        return f'(hh:mm:ss) {hours:02}:{mins:02}:{round(time_taken):02}'
+    if time_taken > 60:
+        mins = int(time_taken / 60)
+        time_taken -= mins * 60
+        return f'{mins}mins{round(time_taken):02}s'
+    return f'{time_taken:.3f}s'
