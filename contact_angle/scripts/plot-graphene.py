e886ea49b7c709efacd341208415c63e814aa167
dwl38 <dwl38@cam.ac.uk>
2025-04-23 16:17:01 +0100

    Refactored plot-graphene to use new helper functions, and also added a plot of mean local inclination angle

--- contact_angle/scripts/plot-graphene.py
+++ contact_angle/scripts/plot-graphene.py
@@ -16,9 +16,10 @@ import os
 import time
 import argparse
 import numpy as np
-import ase.io
 import matplotlib.pyplot as plt
-from contact_angle.util.graphene import inclination_norm_inf_autocor
+from scipy.optimize import curve_fit
+from contact_angle.util import elapsed_time, read_droplet_trajectory
+from contact_angle.util.graphene import calc_inclination_angles
 
 if __name__ == "__main__":
 
@@ -34,10 +35,10 @@ if __name__ == "__main__":
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument('input_file')
     parser.add_argument('-o', '--output', default='graphene.png')
+    parser.add_argument('--index', default=':')
     parser.add_argument('-t', '--max_tau', type=int, default=30)
     parser.add_argument('-N', '--N_x', type=int, default=80)
     parser.add_argument('--N_y', type=int, default=None)
-    parser.add_argument('--max_threads', type=int, default=None)
     parser.add_argument('--no-display', action='store_false', dest='opt_display')
     args = parser.parse_args()
 
@@ -52,62 +53,59 @@ if __name__ == "__main__":
     # Read input file and save coordinates
 
     time_start = time.time()
-    traj = ase.io.iread(args.input_file, index=':')
-    cell_xy = None
-    elem_to_find = None
-    carbons = list()
-    
-    for atoms in traj:
-
-        # Get cell parameters
-        if cell_xy is None:
-            cell_xy = np.array(atoms.cell.cellpar()[0:2])
-        
-        # Check how atomic numbers are assigned in the file
-        if elem_to_find is None:
-            element_numbers = np.unique(atoms.numbers)
-            if np.array_equal(element_numbers, [1, 2, 3]) or np.array_equal(element_numbers, [1,]):
-                elem_to_find = 1
-            elif np.array_equal(element_numbers, [1, 6, 8]) or np.array_equal(element_numbers, [6,]):
-                elem_to_find = 6
-            else:
-                raise RuntimeError('Unidentified atomic numbers in file!')
-            
-        # Read coordinates accordingly
-        carbons.append(atoms.positions[atoms.numbers == elem_to_find])
+    cell_params, _, carbons, _ = read_droplet_trajectory(args.input_file, index=args.index)
+    cell_xy = cell_params[0:2]
+    print(f'Read "{args.input_file}" in {elapsed_time(time_start)}.')
     
     #----------------------------------------------------------------------------------------------
-    # Calculate autocorrelations and display
+    # Calculate local inclination angles and display
 
-    threads = min(32, (os.cpu_count() or 1))
-    if args.max_threads is not None:
-        threads = min(args.max_threads, threads)
-    print(f'Using {threads} workers')
+    time_start = time.time()
+    angles = calc_inclination_angles(carbons, cell_xy, (args.N_x, args.N_y))
+    print(f'Calculated local inclination angles in {elapsed_time(time_start)}.')
 
-    carbons = np.array(carbons)
-    data = inclination_norm_inf_autocor(carbons, cell_xy, args.max_tau, (args.N_x, args.N_y),
-                                        max_threads=threads)
-    
-    time_taken = time.time() - time_start
-    hours = int(time_taken / 3600)
-    time_taken -= 3600 * hours
-    minutes = int(time_taken / 60)
-    time_taken -= 60 * minutes
-    print(f'Took (hh:mm:ss) {hours:02}:{minutes:02}:{round(time_taken):02} of wall time.')
+    time_start = time.time()
+    autocorr = np.zeros((args.max_tau, args.N_x, args.N_y), dtype=float)
+    autocorr[0] = np.mean(np.square(angles), axis=0)
+    for tau in range(1, args.max_tau):
+        autocorr[tau] = np.mean(angles[:-tau] * angles[tau:], axis=0)
+    autocorr[:] /= autocorr[0]
 
-    fig, ax = plt.subplots()
-    fig.set_size_inches(12, 9)
-    artist = ax.imshow(np.swapaxes(data, 0, 1), origin='lower', extent=(-0.5 * cell_xy[0],
-                                                                        0.5 * cell_xy[0],
-                                                                        -0.5 * cell_xy[1],
-                                                                        0.5 * cell_xy[1]))
-    cbar = fig.colorbar(artist, ax=ax)
+    def exp_curve(x, A, k, c):
+        return A * np.exp(-k * x) + c
+
+    tau = np.array(range(args.max_tau))
+    inf_autoc = np.zeros((args.N_x, args.N_y), dtype=float)
+    for i in range(args.N_x):
+        for j in range(args.N_y):
+            try:
+                popt, _ = curve_fit(exp_curve, tau, autocorr[:,i,j], p0=(0.2, 0.5, 0.8))
+                inf_autoc[i,j] = popt[-1]
+            except RuntimeError:
+                inf_autoc[i,j] = 0.8
+    print(f'Calculated autocorrelations in {elapsed_time(time_start)}.')
+
+    fig, ax = plt.subplots(1, 2)
+    fig.set_size_inches(14, 8)
+    extent =  (-0.5 * cell_xy[0], 0.5 * cell_xy[0], -0.5 * cell_xy[1], 0.5 * cell_xy[1])
+
+    im = ax[0].imshow(np.swapaxes(np.mean(angles, axis=0), 0, 1), origin='lower', extent=extent)
+    cbar = fig.colorbar(im, ax=ax[0], fraction=0.046, pad=0.04)
+    cbar.set_label(r'$\langle\theta\rangle_t$')
+    ax[0].set_xlabel(r'x ($\AA$)')
+    ax[0].set_ylabel(r'y ($\AA$)')
+    ax[0].set_title('Time-averaged local inclination angle')
+    ax[0].set_aspect('equal')
+
+    im = ax[1].imshow(np.swapaxes(inf_autoc, 0, 1), origin='lower', extent=extent)
+    cbar = fig.colorbar(im, ax=ax[1], fraction=0.046, pad=0.04)
     cbar.set_label(r'$C_{\theta}(\tau\to\infty)$')
-    ax.set_xlabel(r'x ($\AA$)')
-    ax.set_ylabel(r'y ($\AA$)')
-    ax.set_title('Normalized autocorrelation of local inclination angle')
-    ax.set_aspect('equal')
+    ax[1].set_xlabel(r'x ($\AA$)')
+    ax[1].set_ylabel(r'y ($\AA$)')
+    ax[1].set_title('Normalized autocorrelation of local inclination angle')
+    ax[1].set_aspect('equal')
 
+    fig.tight_layout()
     fig.savefig(args.output, dpi=(3*fig.dpi), bbox_inches='tight', pad_inches=0.05)
     if args.opt_display:
         plt.show()
