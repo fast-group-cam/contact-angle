a99e863f62566d98168aa6e45216d5b373e8de76
dwl38 <dwl38@cam.ac.uk>
2025-04-24 15:54:00 +0100

    Added tests for graphene modules

---
+++ tests/test_graphene.py
@@ -0,0 +1,18 @@
+import numpy as np
+from contact_angle.util import read_droplet_trajectory
+from contact_angle.util.graphene import *
+
+#==================================================================================================
+
+def test():
+
+    cell_params, _, carbons, _ = read_droplet_trajectory('tests/examples/graphene.xyz')
+
+    interp = smooth_sheet(carbons[0], cell_params[0:2])
+    assert np.allclose(interp, np.zeros_like(interp))
+
+    angles = calc_inclination_angles(carbons[0], cell_params[0:2])
+    assert np.allclose(angles, np.zeros_like(angles))
+
+    _, fourier = calc_fourier_coefficients(carbons[0], cell_params[0:2])
+    assert np.allclose(fourier, np.zeros_like(fourier))
---
+++ tests/test_read_droplet_trajectory.py
@@ -0,0 +1,23 @@
+import numpy as np
+from contact_angle.util import read_droplet_trajectory
+
+#==================================================================================================
+
+def test_droplet():
+    params, oxy, car, hyd = read_droplet_trajectory('tests/examples/small-droplet.xyz')
+    assert np.allclose(params, (51.336, 49.4, 50))
+    assert oxy.shape == (1, 130, 3)
+    assert car.shape == (1, 960, 3)
+    assert hyd.shape == (1, 260, 3)
+    assert np.allclose(np.mean(car, axis=(0,1))[2], 0.0)
+    assert np.allclose(np.mean(oxy, axis=(0,1))[0:2], (0.0, 0.0))
+
+#==================================================================================================
+
+def test_graphene():
+    params, oxy, car, hyd = read_droplet_trajectory('tests/examples/graphene.xyz')
+    assert np.allclose(params, (59.892, 59.27770684, 59.58406178))
+    assert oxy.shape == (1, 0, 3)
+    assert car.shape == (1, 1344, 3)
+    assert hyd.shape == (1, 0, 3)
+    assert np.allclose(np.mean(car, axis=(0,1))[2], 0.0)
