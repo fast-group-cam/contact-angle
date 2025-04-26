b53a09c99d123bcdec9daaa73b3b06c40b7b217a
dwl38 <dwl38@cam.ac.uk>
2025-04-26 18:32:50 +0100

    Partial work on total revamp of contact-angle script, WIP (2/?)

--- contact_angle/scripts/contact-angle.py
+++ contact_angle/scripts/contact-angle.py
@@ -27,6 +27,7 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 from contact_angle.util import elapsed_time, read_droplet_trajectory
 from contact_angle.util.droplet import find_interface
+from contact_angle.util.droplet.coarse_grain import COARSE_GRAIN_LENGTH
 from contact_angle.util.droplet.plot import plot_density_xz_slice
 
 #==================================================================================================
@@ -34,10 +35,8 @@ from contact_angle.util.droplet.plot import plot_density_xz_slice
 
 def find_interfaces_and_normals(
         waters: np.ndarray,
-        carbons: np.ndarray,
         search_directions: np.ndarray,
-        z_foot: float,
-        carbon_radius_sq: float
+        z_foot: float
         ) -> tuple[np.ndarray, np.ndarray]:
     """This function takes in a trajectory of water molecules on a graphene sheet, and finds the
 time-averaged Willard-Chandler interface and its normals along a list of planar search directions.
@@ -49,7 +48,7 @@ The inputs are:
     will be collated together.
     - `search_directions`: The directions to the search along, with shape (N_directions, 3); note
     that they should all be perpendicular to the z-axis!
-    - `z_foot`: The defining height of the droplet foot above the graphene sheet.
+    - `z_foot`: The defining height of the droplet foot above the droplet floor.
     - `carbon_radius_sq`: The square of the cutoff radius for searching for nearby carbon atoms
     to define the local sheet z-coordinate.
 
@@ -60,18 +59,13 @@ and normals of the interface points along the search directions."""
     if len(waters.shape) != 3 or waters.shape[-1] != 3:
         raise RuntimeError(f'Unrecognized input shape: waters {waters.shape}, should be ' +
                            '(N_frames, N_water, 3)')
-    if len(carbons.shape) != 3 or carbons.shape[-1] != 3:
-        raise RuntimeError(f'Unrecognized input shape: carbons {carbons.shape}, should be ' +
-                           '(N_frames, N_carbon, 3)')
-    N_frames = waters.shape[0]
-    if N_frames != carbons.shape[0]:
-        raise RuntimeError(f'Inconsistent number of frames in waters ({N_frames}) and carbons ' +
-                           f'({carbons.shape[0]})!')
-    flat_carbons = carbons.reshape(-1, 3)
 
     # Calculate droplet CoM and floor
     CoM = np.mean(waters, axis=(0, 1))
+    droplet_h = find_interface(waters, CoM, (0, 0, 1))[2]
     droplet_floor = find_interface(waters, CoM, (0, 0, -1))[2]
+    if droplet_h - droplet_floor < z_foot:
+        warnings.warn('Droplet is too short at the CoM, bad behaviour may occur')
     
     # Iterate through search directions
     interfaces = list()
@@ -81,23 +75,19 @@ and normals of the interface points along the search directions."""
         # First guess of interface
         inter = find_interface(waters, (0, 0, droplet_floor + z_foot), search_dir)
 
-        # Find z-coordinate of graphene sheet underneath first guess
-        nearby = flat_carbons[np.sum(np.square(flat_carbons[:,0:2] - inter[0:2]),
-                                     axis=-1) < carbon_radius_sq]
-        sheet_z = np.mean(nearby[:,2])
+        # Find z-coordinate of droplet floor underneath first guess
+        local_floor = find_interface(waters, inter, (0, 0, -1))[2]
 
         # Second guess of interface (and repeat refinement)
-        #inter = find_interface(waters, (inter[0] - search_dir[0], inter[1] - search_dir[1],
-        #                                sheet_z + z_foot), search_dir)
-        inter = find_interface(waters, (0, 0, sheet_z + z_foot), search_dir)
-        nearby = flat_carbons[np.sum(np.square(flat_carbons[:,0:2] - inter[0:2]),
-                                     axis=-1) < carbon_radius_sq]
-        sheet_z = np.mean(nearby[:,2])
+        step_back = min(np.dot(inter, search_dir), 2 * COARSE_GRAIN_LENGTH) * search_dir
+        inter = find_interface(waters, (inter[0] - step_back[0], inter[1] - step_back[1],
+                                        local_floor + z_foot), search_dir)
+        local_floor = find_interface(waters, inter, (0, 0, -1))[2]
 
         # Third and final guess of interface
-        #inter, norm = find_interface(waters, (inter[0] - search_dir[0], inter[1] - search_dir[1],
-        #                                      sheet_z + z_foot), search_dir, calc_normal=True)
-        inter, norm = find_interface(waters, (0, 0, sheet_z + z_foot), search_dir, calc_normal=True)
+        step_back = min(np.dot(inter, search_dir), 2 * COARSE_GRAIN_LENGTH) * search_dir
+        inter, norm = find_interface(waters, (inter[0] - step_back[0], inter[1] - step_back[1],
+                                              local_floor + z_foot), search_dir, calc_normal=True)
         interfaces.append(inter)
         normals.append(norm)
 
@@ -111,14 +101,10 @@ if __name__ == "__main__":
     #----------------------------------------------------------------------------------------------
     # Script default parameters
 
-    N_AZIMUTHS = 60    # Number of azimuthal directions to analyze per frame
-    Z_FOOT = 5         # Height of the droplet foot above the graphene sheet (in angstroms)
-    MAX_TAU = 25       # Maximum timescale to calculate autocorrelations (in number of frames)
-
-    from contact_angle.util.droplet.coarse_grain import (COARSE_GRAIN_LENGTH,
-                                                         SLICING_CUTOFF,
-                                                         BULK_DENSITY)
-    SLICING_WIDTH = SLICING_CUTOFF * COARSE_GRAIN_LENGTH
+    N_AZIMUTHS = 60       # Number of azimuthal directions to analyze per frame
+    Z_FOOT = 5.0          # Height of the droplet foot above the graphene sheet (in angstroms)
+    MAX_TAU = 25          # Maximum timescale to calculate autocorrelations (in number of frames)
+    N_SPHERE_PTS = 100    # Number of points to use to find best-fit spherical top
 
     from contact_angle.util.graphene.angle import CUTOFF_RADIUS as CARBON_RADIUS
     CARBON_RADIUS_SQ = CARBON_RADIUS**2
@@ -189,6 +175,16 @@ if __name__ == "__main__":
             mu = np.mean(data)
             return np.array((1 - mu, np.inf, mu))
 
+    def sphere_fit(points):
+        A_mat = np.empty((points.shape[0], 4), dtype=float)
+        A_mat[:,0:3] = 2 * points
+        A_mat[:,3] = 1
+        f_vec = np.empty((points.shape[0], 1), dtype=float)
+        f_vec[:,0] = np.sum(np.square(points), axis=-1)
+        c_vec, _, _, _ = np.linalg.lstsq(A_mat, f_vec)
+        radius = np.sqrt(np.sum(np.square(c_vec[0:3,0])) + c_vec[3,0])
+        return (radius, c_vec[0:3,0])
+
     def plot_against_time_and_azimuth(fig, ax, data, title, var_label):
         N_x = data.shape[0]
         N_y = data.shape[1]
@@ -239,9 +235,7 @@ if __name__ == "__main__":
 
     for f in range(N_frames):
 
-        interfaces, normals = find_interfaces_and_normals(waters[f:f+1], carbons[f:f+1],
-                                                          search_directions, z_foot=args.z_foot,
-                                                          carbon_radius_sq=CARBON_RADIUS_SQ)
+        interfaces, normals = find_interfaces_and_normals(waters[f:f+1], search_directions, args.z_foot)
         
         if args.local:
             for i, inter in enumerate(interfaces):
@@ -362,39 +356,52 @@ if __name__ == "__main__":
     azi = np.linspace(0, 2 * np.pi, 12, endpoint=False)
     search_directions = np.c_[np.cos(azi), np.sin(azi), np.zeros_like(azi)]
     search_perp = np.c_[-np.sin(azi), np.cos(azi)]
-    interfaces, normals = find_interfaces_and_normals(waters, carbons, search_directions,
-                                                      z_foot=args.z_foot,
-                                                      carbon_radius_sq=CARBON_RADIUS_SQ)
+    interfaces, normals = find_interfaces_and_normals(waters, search_directions, args.z_foot)
     if args.local:
         flat_carbons = carbons.reshape(-1, 3)
+        local_carbons_c = np.empty((interfaces.shape[0], 3), dtype=float)
+        local_carbons_n = np.empty((interfaces.shape[0], 3), dtype=float)
         contact_angles = np.empty((12,), dtype=float)
         for i, inter in enumerate(interfaces):
             nearby = flat_carbons[np.sum(np.square(flat_carbons[:,0:2] - inter[0:2]), axis=-1) < CARBON_RADIUS_SQ]
-            local_norm = np.linalg.svd(nearby - np.mean(nearby, axis=0), full_matrices=False)[2][-1]
-            local_norm /= np.linalg.norm(local_norm)
-            if local_norm[2] < 0:
-                local_norm *= -1
-            contact_angles[i] = np.arccos(np.dot(normals[i], local_norm)) * 180 / np.pi
+            local_carbons_c[i] = np.mean(nearby, axis=0)
+            local_carbons_n[i] = np.linalg.svd(nearby - local_carbons_c[i], full_matrices=False)[2][-1]
+            local_carbons_n[i] /= np.linalg.norm(local_carbons_n[i])
+            if local_carbons_n[i,2] < 0:
+                local_carbons_n[i] *= -1
+            contact_angles[i] = np.arccos(np.dot(normals[i], local_carbons_n[i])) * 180 / np.pi
     else:
         contact_angles = np.arccos(np.dot(normals, (0, 0, 1))) * 180 / np.pi
 
     proj_normals = np.power(np.sum(np.square(normals[:,0:2]), axis=-1), -0.5)[:,None] * normals[:,0:2]
     ooplane_angles = np.arcsin(np.sum(proj_normals * search_perp, axis=-1)) * 180 / np.pi
 
-    # TODO: implement spherical radius calculation
-
     time_start_2 = time.time()
     print(f'    - computed time-averaged interface in {elapsed_time(time_start_1)}.')
-    print('    - plotting time-averaged density functions...', end='')
+    print('    - finding best-fit sphere for upper surface...', end='')
 
-    # TODO: plot normals, plot spherical best-fit
+    phi = np.random.random(N_SPHERE_PTS) * 2 * np.pi
+    tau = np.random.random(N_SPHERE_PTS)
+    search_directions = np.c_[np.sqrt(1 - np.square(tau)) * np.cos(phi), np.sqrt(1 - np.square(tau)) * np.sin(phi), tau]
+    sphere_points = list()
+    CoM = np.mean(waters, axis=(0,1))
+    for i in range(N_SPHERE_PTS):
+        sphere_points.append(find_interface(waters, CoM, search_directions[i]))
+    sphere_points = np.array(sphere_points)
+    sphere_r, sphere_c = sphere_fit(sphere_points)
+
+    print(f'done in {elapsed_time(time_start_2)}.')
+    time_start_2 = time.time()
+    print('    - plotting time-averaged density functions...', end='')
 
     fig, ax = plt.subplots(2, 3)
     fig.set_size_inches(15, 6)
     interval = max(int(N_frames * N_water / 2e5), 1)
     for i in range(2):
         for j in range(3):
-            angle = azi[(3 * i) + j]
+
+            idx = (3 * i) + j
+            angle = azi[idx]
             rot_matrix = np.array(((np.cos(angle),  np.sin(angle), 0.0),
                                    (-np.sin(angle), np.cos(angle), 0.0),
                                    (0.0,            0.0,           1.0)))
@@ -402,11 +409,41 @@ if __name__ == "__main__":
             rot_carbons = np.einsum('kl,ijl->ijk', rot_matrix, carbons[::interval])
             plot_density_xz_slice(rot_waters, rot_carbons, ax[i][j], show_interface=True,
                                   color_inter = (1.0, 0.0, 1.0, 0.5))
+            
+            for k in (0, 6):
+                if args.local:
+                    rot_carbon_c = rot_matrix @ local_carbons_c[idx + k]
+                    rot_carbon_n = rot_matrix @ local_carbons_n[idx + k]
+                    a_x = rot_carbon_c[0] - CARBON_RADIUS
+                    a_z = rot_carbon_c[2] + (CARBON_RADIUS * rot_carbon_n[0] / rot_carbon_n[2])
+                    b_x = rot_carbon_c[0] + CARBON_RADIUS
+                    b_z = rot_carbon_c[2] - (CARBON_RADIUS * rot_carbon_n[0] / rot_carbon_n[2])
+                    ax[i][j].plot((a_x, b_x), (a_z, b_z), 'k-')
+                rot_inter = rot_matrix @ interfaces[idx + k]
+                rot_norm = rot_matrix @ normals[idx + k]
+                a_x = rot_inter[0] + (rot_inter[2] * rot_norm[2] / rot_norm[0])
+                b_x = rot_inter[0] - (2 * rot_inter[2] * rot_norm[2] / rot_norm[0])
+                ax[i][j].plot((a_x, b_x), (0.0, 3 * rot_inter[2]), '-', color=(1.0, 0.0, 1.0))
+            ax[i][j].plot((0.0,), (CoM[2],), '.', color=(1.0, 0.0, 1.0))
+
+            rot_sphere_c = rot_matrix @ sphere_c
+            proj_sphere_r_sq = (sphere_r**2) - (rot_sphere_c[1]**2)
+            if proj_sphere_r_sq > 0.0:
+                a_x = 0.5 * np.sqrt(proj_sphere_r_sq)
+                b_x = np.linspace(-a_x, a_x, 100)
+                ax[i][j].plot(rot_sphere_c[0] + b_x, rot_sphere_c[2] + np.sqrt(proj_sphere_r_sq - np.square(b_x)),
+                              '-.', color=(0.0, 0.5, 0.0, 0.5))
+            ax[i][j].plot((rot_sphere_c[0],), (rot_sphere_c[2],), '.', color=(0.0, 0.5, 0.0))
+
             ax[i][j].set_title(r'$\varphi\;=\;' + f'{(angle * 180 / np.pi):.0f}' + r'\degree$')
+
     fig.suptitle('Azimuthal cross-sections of time-averaged droplet')
     fig.savefig(os.path.join(args.output, 'ave_cross_sections.png'), dpi=(3*fig.dpi),
                 bbox_inches='tight', pad_inches=0.05)
     
+    print(f'done in {elapsed_time(time_start_2)}.')
+    print(f'Done in {elapsed_time(time_start_1)}.')
+
     log_file.write('-------------------------\n')
     log_file.write(' Time-averaged interface\n')
     log_file.write('-------------------------\n\n')
@@ -416,8 +453,9 @@ if __name__ == "__main__":
     log_file.write(f'Mean out-of-plane angle = {np.mean(ooplane_angles)} [deg]\n')
     log_file.write(f'Median out-of-plane angle = {np.median(ooplane_angles)} [deg]\n')
     log_file.write(f'Out-of-plane angle stdev = {np.std(ooplane_angles)} [deg]\n\n')
-
-    print(f'Done in {elapsed_time(time_start_1)}.')
+    log_file.write(f'Center-of-mass z-height = {CoM[2]} [A]\n')
+    log_file.write(f'Best-fit spherical center z-height = {sphere_c[2]} [A]\n')
+    log_file.write(f'Best-fit spherical radius = {sphere_r} [A]\n\n')
 
     #----------------------------------------------------------------------------------------------
     # Calculate time-averaged interface across all frames
