917702e27241fcfc8f8aa82d692b287cd147dba0
dwl38 <dwl38@cam.ac.uk>
2025-05-19 15:43:27 +0100

    Added best_fit_axial_sphere functionality for constrained sphere-fitting

--- droplet_graphene_analysis/util/__init__.py
+++ droplet_graphene_analysis/util/__init__.py
@@ -40,7 +40,7 @@ def best_fit_sphere(points: np.ndarray, d: int = None) -> tuple[float, np.ndarra
     -------
     a : float
         The radius of the best-fit sphere; guaranteed positive.
-    c: ndarray
+    c : ndarray
         The Cartesian coordinates of the center of the best-fit sphere, with shape (d,).
     """
     dims = (points.shape[-1] if d is None else int(d))
@@ -52,3 +52,37 @@ def best_fit_sphere(points: np.ndarray, d: int = None) -> tuple[float, np.ndarra
     c_vec, _, _, _ = np.linalg.lstsq(A_mat, f_vec, rcond=None)
     a = np.sqrt(np.sum(np.square(c_vec[0:dims,0])) + c_vec[dims,0])
     return (a, c_vec[0:dims,0])
+
+#==================================================================================================
+
+def best_fit_axial_sphere(points: np.ndarray) -> tuple[float, float]:
+    """Calculates the best-fit (d-1)-dimensional sphere constrained along the last axis, i.e. the
+    surface defined by :math:`|r - c| = a` for centre :math:`c = (0, 0, ..., c_z)`, for a set of
+    d-dimensional points. The sphere is fitted by minimizing the squares of the points' squared
+    distances from the centre minus the squared radius, i.e. the sum of :math:`(|r_i - c|^2 - a^2)^2`.
+
+    Parameters
+    ----------
+    points : ndarray
+        Cartesian coordinates of the points to fit to, with shape (N_points, d).
+
+    Returns
+    -------
+    a : float
+        The radius of the best-fit sphere; guaranteed positive.
+    c_z : float
+        The last Cartesian coordinate of the center of the best-fit sphere.
+
+    Notes
+    -----
+    Unlike `best_fit_sphere`, the dimension d cannot be specified for this function, and will
+    always use the full shape of `points`.
+    """
+    A_mat = np.empty((points.shape[0], 2), dtype=float)
+    A_mat[:,0] = 2 * points[:,-1]
+    A_mat[:,1] = 1
+    f_vec = np.empty((points.shape[0], 1), dtype=float)
+    f_vec[:,0] = np.sum(points**2, axis=-1)
+    c_vec, _, _, _ = np.linalg.lstsq(A_mat, f_vec, rcond=None)
+    a = np.sqrt(np.sum(np.square(c_vec[0,0])) + c_vec[1,0])
+    return (a, c_vec[0,0])
--- tests/test_fit_sphere.py
+++ tests/test_fit_sphere.py
@@ -1,5 +1,5 @@
 import numpy as np
-from droplet_graphene_analysis.util import best_fit_sphere
+from droplet_graphene_analysis.util import best_fit_sphere, best_fit_axial_sphere
 
 #==================================================================================================
 
@@ -36,5 +36,25 @@ def test_3d():
         r = a0[n] + (noise * (np.random.random(N_pts) - 0.5))
         pts = c0[n] + np.c_[r * np.cos(phi) * sine, r * np.sin(phi) * sine, r * cosine]
         a, c = best_fit_sphere(pts, d=3)
+        assert np.allclose(a, a0[n], atol=noise)
+        assert np.allclose(c, c0[n], atol=noise)
+
+#==================================================================================================
+
+def test_3d_constrained():
+
+    N_tests = 50
+    N_pts = 100
+    noise = 0.01
+    c0 = (2.0 * np.random.random(N_tests)) - 1.0
+    a0 = 1.0 + (9.0 * np.random.random(N_tests))
+
+    for n in range(N_tests):
+        phi = 2 * np.pi * np.random.random(N_pts)
+        cosine = (2.0 * np.random.random(N_pts)) - 1.0
+        sine = np.sqrt(1 - (cosine**2))
+        r = a0[n] + (noise * (np.random.random(N_pts) - 0.5))
+        pts = np.array([0.0, 0.0, c0[n]]) + np.c_[r * np.cos(phi) * sine, r * np.sin(phi) * sine, r * cosine]
+        a, c = best_fit_axial_sphere(pts)
         assert np.allclose(a, a0[n], atol=noise)
         assert np.allclose(c, c0[n], atol=noise)
